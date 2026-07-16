20ec602e33a12ed1112a8da4a9a7a8ae35b72677
AKASH KUMAR <149068690+akashkumar181@users.noreply.github.com>
2026-07-16 10:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.37%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string makeGood(string s) {
+        string result = "";
+        
+        for (int i = 0; i < s.length(); i++) {
+            char current_char = s[i];
+            
+            if (result.length() > 0) {
+                char last_char = result.back();
+                
+                if (last_char != current_char && tolower(last_char) == tolower(current_char)) {
+                    result.pop_back(); 
+                    continue;          
+                }
+            }
+            
+       
+            result.push_back(current_char);
+        }
+        
+        return result;
+    }
+};
